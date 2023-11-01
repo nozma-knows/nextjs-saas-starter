ed5f7d1abf054ebaba863dc486d2e902f1c51ed1
Noah Milberger <n.milberger@gmail.com>
2023-11-01 15:23:46 -0400

    Updated UI for SignOutButton.

--- src/components/ui/Navbar/SignOutButton.tsx
+++ src/components/ui/Navbar/SignOutButton.tsx
@@ -1,23 +1,16 @@
 'use client';
 
 import { useSupabase } from '@/app/supabase-provider';
+import { Button } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
-
-import s from './Navbar.module.css';
 
 export default function SignOutButton() {
   const router = useRouter();
   const { supabase } = useSupabase();
+
   const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
-  return (
-    <button
-      className={s.link}
-      onClick={handleSignOut}
-    >
-      Sign out
-    </button>
-  );
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+  return <Button onClick={handleSignOut}>Sign out</Button>;
 }
