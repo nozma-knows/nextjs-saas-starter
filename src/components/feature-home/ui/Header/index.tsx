a075efef9efa68dd16a452c4e280bfef2d0e78ff
Noah Milberger <n.milberger@gmail.com>
2023-11-01 15:04:08 -0400

    Created feature-home folder in components.

---
+++ src/components/feature-home/ui/Header/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Box, Button, Flex, Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { FC } from 'react';
+
+interface Props {}
+
+const Header: FC<Props> = () => {
+  const router = useRouter();
+  const title = `App Name`;
+  const subtitle = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae est.`;
+  return (
+    <Flex gap="8" py="16" alignItems={'center'}>
+      <Stack textColor={'white'}>
+        <h1 className="text-5xl font-bold">{title}</h1>
+        <p className="text-lg">{subtitle}</p>
+        <Flex justifyContent={'start'} pt={4}>
+          <Button onClick={() => router.push('/signin')} w="fit">
+            Get started today
+          </Button>
+        </Flex>
+      </Stack>
+      <Box w="full" h="full" minH={'80'} bg="whiteAlpha.300" rounded="md" />
+    </Flex>
+  );
+};
+
+export default Header;
