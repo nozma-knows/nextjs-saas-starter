9d7b5df6ec28154078e72aa1e2da9cd9d284f51e
Noah Milberger <n.milberger@gmail.com>
2023-11-01 15:03:03 -0400

    Added Header and Instructions to main page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Pricing from '@/components/Pricing';
 import {
   getSession,
   getSubscription,
   getActiveProductsWithPrices
 } from '@/app/supabase-server';
+import Pricing from '@/components/Pricing';
+import Header from '@/components/feature-home/ui/Header';
+import Instructions from '@/components/feature-home/ui/Intstructions';
+import { Stack } from '@chakra-ui/react';
 
 export default async function PricingPage() {
   const [session, products, subscription] = await Promise.all([
@@ -13,11 +16,15 @@ export default async function PricingPage() {
   ]);
 
   return (
-    <Pricing
-      session={session}
-      user={session?.user}
-      products={products}
-      subscription={subscription}
-    />
+    <Stack w="full">
+      <Header />
+      <Instructions />
+      <Pricing
+        session={session}
+        user={session?.user}
+        products={products}
+        subscription={subscription}
+      />
+    </Stack>
   );
 }
