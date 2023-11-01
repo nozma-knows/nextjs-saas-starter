45262441297aa228dd33a670e48ca6bb03e9ed49
Noah Milberger <n.milberger@gmail.com>
2023-11-01 15:03:27 -0400

    Updated UI for Navbar.

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -13,46 +13,11 @@ export default async function Navbar() {
   const {
     data: { user }
   } = await supabase.auth.getUser();
-  // return (
-  //   <nav className={s.root}>
-  //     <a href="#skip" className="sr-only focus:not-sr-only">
-  //       Skip to content
-  //     </a>
-  //     <div className="max-w-6xl px-6 mx-auto">
-  //       <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-  //         <div className="flex items-center flex-1">
-  //           <Link href="/" className={s.logo} aria-label="Logo">
-  //             <Logo />
-  //           </Link>
-  //           <nav className="hidden ml-6 space-x-2 lg:block">
-  //             <Link href="/" className={s.link}>
-  //               Pricing
-  //             </Link>
-  //             {user && (
-  //               <Link href="/account" className={s.link}>
-  //                 Account
-  //               </Link>
-  //             )}
-  //           </nav>
-  //         </div>
-  //         <div className="flex justify-end flex-1 space-x-8">
-  //           {user ? (
-  //             <SignOutButton />
-  //           ) : (
-  //             <Link href="/signin" className={s.link}>
-  //               Sign in
-  //             </Link>
-  //           )}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </nav>
-  // );
 
   return (
     <Flex
       flexDir={'row'}
-      w={'100%'}
+      w="full"
       alignItems={'center'}
       zIndex={'1500'}
       justifyContent={'space-between'}
@@ -86,34 +51,6 @@ export default async function Navbar() {
           Sign in
         </Link>
       )}
-      {/* <div className="max-w-6xl px-6 mx-auto">
-          <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-            <div className="flex items-center flex-1">
-              <Link href="/" className={s.logo} aria-label="Logo">
-                <Logo />
-              </Link>
-              <nav className="hidden ml-6 space-x-2 lg:block">
-                <Link href="/" className={s.link}>
-                  Pricing
-                </Link>
-                {user && (
-                  <Link href="/account" className={s.link}>
-                    Account
-                  </Link>
-                )}
-              </nav>
-            </div>
-            <div className="flex justify-end flex-1 space-x-8">
-              {user ? (
-                <SignOutButton />
-              ) : (
-                <Link href="/signin" className={s.link}>
-                  Sign in
-                </Link>
-              )}
-            </div>
-          </div>
-        </div> */}
     </Flex>
   );
 }
