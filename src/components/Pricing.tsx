7bf4cb43aa31c16556e70d39327e455921f5cb7b
Noah Milberger <n.milberger@gmail.com>
2023-11-02 08:53:43 -0400

    Updated UI for Pricing page.

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,9 +1,17 @@
 'use client';
 
-import Button from '@/components/ui/Button';
 import { Database } from '@/types_db';
 import { postData } from '@/utils/helpers';
 import { getStripe } from '@/utils/stripe-client';
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Link,
+  Stack,
+  Text
+} from '@chakra-ui/react';
 import { Session, User } from '@supabase/supabase-js';
 import cn from 'classnames';
 import { useRouter } from 'next/navigation';
@@ -28,6 +36,37 @@ interface Props {
   products: ProductWithPrices[];
   subscription: SubscriptionWithProduct | null;
 }
+
+const NoProductsView = () => {
+  return (
+    <Stack px={4} py={8}>
+      <Text
+        fontSize={['4xl', '6xl']}
+        fontWeight="extrabold"
+        color="white"
+        textAlign="center"
+      >
+        Pricing Plans
+      </Text>
+      <Box fontSize="6xl" fontWeight="extrabold" textAlign="center">
+        {/* <Text color="white">
+        No subscription pricing plans found. Create them in your{' '}
+      </Text> */}
+        <span className="text-white">
+          No subscription pricing plans found. Create them in your{' '}
+          <Link
+            href="https://dashboard.stripe.com/products"
+            target="_blank"
+            color="blue"
+          >
+            Stripe Dashboard
+          </Link>
+          .
+        </span>
+      </Box>
+    </Stack>
+  );
+};
 
 type BillingInterval = 'lifetime' | 'year' | 'month';
 
@@ -72,248 +111,68 @@ export default function Pricing({
     }
   };
 
-  if (!products.length)
-    return (
-      <section className="bg-black">
-        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-          <div className="sm:flex sm:flex-col sm:align-center"></div>
-          <p className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            No subscription pricing plans found. Create them in your{' '}
-            <a
-              className="text-pink-500 underline"
-              href="https://dashboard.stripe.com/products"
-              rel="noopener noreferrer"
-              target="_blank"
+  if (!products.length) return <NoProductsView />;
+
+  return (
+    <Stack px={4} py={8} textAlign="center" alignItems={'center'} gap={4}>
+      <Text
+        fontSize={['4xl', '6xl']}
+        fontWeight="extrabold"
+        color="white"
+        textAlign="center"
+      >
+        Pricing Plans
+      </Text>
+      <Text maxW="2xl" fontSize={['xl', '2xl']} color="gray.50">
+        Start building for free, then add a site plan to go live. Account plans
+        unlock additional features.
+      </Text>
+      <Flex w="full" gap={4}>
+        {products.map((product) => {
+          const price = product?.prices?.find(
+            (price) => price.interval === billingInterval
+          );
+          if (!price) return null;
+          const priceString = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: price.currency!,
+            minimumFractionDigits: 0
+          }).format((price?.unit_amount || 0) / 100);
+          return (
+            <Stack
+              w="full"
+              h="full"
+              bg="blackAlpha.400"
+              border="2px"
+              borderColor="whiteAlpha.400"
+              gap={'4'}
+              p={'4'}
+              rounded={'md'}
+              color="white"
             >
-              Stripe Dashboard
-            </a>
-            .
-          </p>
-        </div>
-        <LogoCloud />
-      </section>
-    );
-
-  if (products.length === 1)
-    return (
-      <section className="bg-black">
-        <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-          <div className="sm:flex sm:flex-col sm:align-center">
-            <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-              Pricing Plans
-            </h1>
-            <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-              Start building for free, then add a site plan to go live. Account
-              plans unlock additional features.
-            </p>
-            <div className="relative flex self-center mt-12 border rounded-lg bg-zinc-900 border-zinc-800">
-              <div className="border border-pink-500 border-opacity-50 divide-y rounded-lg shadow-sm bg-zinc-900 divide-zinc-600">
-                <div className="p-6 py-2 m-1 text-2xl font-medium text-white rounded-md shadow-sm border-zinc-800 whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8">
-                  {products[0].name}
-                </div>
-              </div>
-            </div>
-            <div className="mt-6 space-y-4 sm:mt-12 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
-              {products[0].prices?.map((price) => {
-                const priceString =
-                  price.unit_amount &&
-                  new Intl.NumberFormat('en-US', {
-                    style: 'currency',
-                    currency: price.currency!,
-                    minimumFractionDigits: 0
-                  }).format(price.unit_amount / 100);
-
-                return (
-                  <div
-                    key={price.interval}
-                    className="divide-y rounded-lg shadow-sm divide-zinc-600 bg-zinc-900"
-                  >
-                    <div className="p-6">
-                      <p>
-                        <span className="text-5xl font-extrabold white">
-                          {priceString}
-                        </span>
-                        <span className="text-base font-medium text-zinc-100">
-                          /{price.interval}
-                        </span>
-                      </p>
-                      <p className="mt-4 text-zinc-300">{price.description}</p>
-                      <Button
-                        variant="slim"
-                        type="button"
-                        disabled={false}
-                        loading={priceIdLoading === price.id}
-                        onClick={() => handleCheckout(price)}
-                        className="block w-full py-2 mt-12 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900 "
-                      >
-                        {products[0].name ===
-                        subscription?.prices?.products?.name
-                          ? 'Manage'
-                          : 'Subscribe'}
-                      </Button>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <LogoCloud />
-        </div>
-      </section>
-    );
-
-  // TODO: Update UI below to look better
-  return (
-    <section className="bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:flex-col sm:align-center">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Pricing Plans
-          </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Start building for free, then add a site plan to go live. Account
-            plans unlock additional features.
-          </p>
-          <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
-            {intervals.includes('month') && (
-              <button
-                onClick={() => setBillingInterval('month')}
-                type="button"
-                className={`${
-                  billingInterval === 'month'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+              <Flex gap="4">
+                <Text fontSize="xl">{product.name}</Text>
+              </Flex>
+              <Flex alignItems="flex-end" justifyContent={'center'} p={'0'}>
+                <Text fontWeight="bold" fontSize="4xl">
+                  {priceString}
+                </Text>
+                <Text pb="2">{`/${billingInterval}`}</Text>
+              </Flex>
+              {product.description && (
+                <Text textAlign="start">{product.description}</Text>
+              )}
+              <Button
+                isDisabled={!session}
+                isLoading={priceIdLoading === price.id}
+                onClick={() => handleCheckout(price)}
               >
-                Monthly billing
-              </button>
-            )}
-            {intervals.includes('year') && (
-              <button
-                onClick={() => setBillingInterval('year')}
-                type="button"
-                className={`${
-                  billingInterval === 'year'
-                    ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                    : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
-              >
-                Yearly billing
-              </button>
-            )}
-          </div>
-        </div>
-        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-3">
-          {products.map((product) => {
-            const price = product?.prices?.find(
-              (price) => price.interval === billingInterval
-            );
-            if (!price) return null;
-            const priceString = new Intl.NumberFormat('en-US', {
-              style: 'currency',
-              currency: price.currency!,
-              minimumFractionDigits: 0
-            }).format((price?.unit_amount || 0) / 100);
-            return (
-              <div
-                key={product.id}
-                className={cn(
-                  'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
-                  {
-                    'border border-pink-500': subscription
-                      ? product.name === subscription?.prices?.products?.name
-                      : product.name === 'Freelancer'
-                  }
-                )}
-              >
-                <div className="p-6">
-                  <h2 className="text-2xl font-semibold leading-6 text-white">
-                    {product.name}
-                  </h2>
-                  <p className="mt-4 text-zinc-300">{product.description}</p>
-                  <p className="mt-8">
-                    <span className="text-5xl font-extrabold white">
-                      {priceString}
-                    </span>
-                    <span className="text-base font-medium text-zinc-100">
-                      /{billingInterval}
-                    </span>
-                  </p>
-                  <Button
-                    variant="slim"
-                    type="button"
-                    disabled={!session}
-                    loading={priceIdLoading === price.id}
-                    onClick={() => handleCheckout(price)}
-                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
-                  >
-                    {subscription ? 'Manage' : 'Subscribe'}
-                  </Button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <LogoCloud />
-      </div>
-    </section>
-  );
-}
-
-// TODO: Add Sync labs logo to cloud
-function LogoCloud() {
-  return (
-    <div>
-      <p className="mt-24 text-xs uppercase text-zinc-400 text-center font-bold tracking-[0.3em]">
-        Built using
-      </p>
-      <div className="flex flex-col items-center my-12 space-y-4 sm:mt-8 sm:space-y-0 md:mx-auto md:max-w-2xl sm:grid sm:gap-6 sm:grid-cols-5">
-        <div className="flex items-center justify-start">
-          <a href="https://synclabs.so" aria-label="Synclabs.so Link">
-            <img
-              src="/synclabs.svg"
-              alt="Next.js Logo"
-              className="h-12 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://nextjs.org" aria-label="Next.js Link">
-            <img
-              src="/nextjs.svg"
-              alt="Next.js Logo"
-              className="h-12 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://vercel.com" aria-label="Vercel.com Link">
-            <img
-              src="/vercel.svg"
-              alt="Vercel.com Logo"
-              className="h-6 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://stripe.com" aria-label="stripe.com Link">
-            <img
-              src="/stripe.svg"
-              alt="stripe.com Logo"
-              className="h-12 text-white"
-            />
-          </a>
-        </div>
-        <div className="flex items-center justify-start">
-          <a href="https://supabase.io" aria-label="supabase.io Link">
-            <img
-              src="/supabase.svg"
-              alt="supabase.io Logo"
-              className="h-10 text-white"
-            />
-          </a>
-        </div>
-      </div>
-    </div>
+                {subscription ? 'Manage' : 'subscribe'}
+              </Button>
+            </Stack>
+          );
+        })}
+      </Flex>
+    </Stack>
   );
 }
