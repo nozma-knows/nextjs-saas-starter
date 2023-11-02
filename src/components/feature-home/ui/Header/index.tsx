704a4d4a941ef6f419c6e6fe356c08e85b54599f
Noah Milberger <n.milberger@gmail.com>
2023-11-01 21:38:14 -0400

    Made Header component mobile friendly.

--- src/components/feature-home/ui/Header/index.tsx
+++ src/components/feature-home/ui/Header/index.tsx
@@ -11,17 +11,30 @@ const Header: FC<Props> = () => {
   const title = `App Name`;
   const subtitle = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae est.`;
   return (
-    <Flex gap="8" py="16" alignItems={'center'}>
+    <Flex
+      gap="8"
+      px={8}
+      py="16"
+      alignItems={['start', 'start', 'start', 'center']}
+      direction={['column', 'column', 'column', 'row']}
+    >
       <Stack textColor={'white'}>
         <h1 className="text-5xl font-bold">{title}</h1>
         <p className="text-lg">{subtitle}</p>
-        <Flex justifyContent={'start'} pt={4}>
+        <Flex justifyContent={['center', 'center', 'center', 'start']} pt={4}>
           <Button onClick={() => router.push('/signin')} w="fit">
             Get started today
           </Button>
         </Flex>
       </Stack>
-      <Box w="full" h="full" minH={'80'} bg="whiteAlpha.300" rounded="md" />
+      <Box
+        w="full"
+        // maxW="6xl"
+        h="full"
+        minH={'80'}
+        bg="whiteAlpha.300"
+        rounded="md"
+      />
     </Flex>
   );
 };
