fbe3e2b80cfc779200cd2d919f59670289bef516
Noah Milberger <n.milberger@gmail.com>
2023-11-02 08:54:01 -0400

    Updated UI for Footer.

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import LogoCloud from '../LogoCloud';
 import logo from '@/assets/logo.png';
 import GitHub from '@/components/icons/GitHub';
 import Logo from '@/components/icons/Logo';
@@ -6,6 +7,35 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FaGithub } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+
+const builtWithLogos = [
+  {
+    label: 'Synclabs.so',
+    link: 'https://synclabs.so',
+    img: '/synclabs.svg'
+  },
+  {
+    label: 'Next.js',
+    link: 'https://nextjs.org',
+    img: '/nextjs.svg'
+  },
+  {
+    label: 'Vercel',
+    link: 'https://vercel.com',
+    img: '/vercel.svg'
+  },
+
+  {
+    label: 'Supabase',
+    link: 'https://supabase.io',
+    img: '/supabase.svg'
+  },
+  {
+    label: 'Stripe',
+    link: 'https://stripe.com',
+    img: '/stripe.svg'
+  }
+];
 
 export default function Footer() {
   return (
@@ -17,25 +47,8 @@ export default function Footer() {
       borderTop="1px"
       borderColor="whiteAlpha.300"
     >
-      <Link href={'/'}>
-        <Flex
-          justifyContent={'center'}
-          alignItems={'center'}
-          h="8"
-          aspectRatio={1 / 1}
-        >
-          <Box w="100%" h="100%" position={'relative'}>
-            <Image
-              src={logo}
-              alt={'Synchronicity Labs Logo'}
-              fill
-              style={{
-                objectFit: 'contain'
-              }}
-            />
-          </Box>
-        </Flex>
-      </Link>
+      <Box />
+      <LogoCloud title="Built with" logos={builtWithLogos} />
       <Flex gap={8}>
         <Link href="https://twitter.com/synclabs_ai">
           <Box color="white" fontSize="3xl">
