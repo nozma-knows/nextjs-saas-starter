7859146b0f1e021b32e94c08f23d91d5d1d9a012
Noah Milberger <n.milberger@gmail.com>
2023-11-03 08:50:01 -0400

    Removed src folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'pages/**/*.{ts,tsx}'
+  ],
   theme: {
     extend: {
       fontFamily: {
