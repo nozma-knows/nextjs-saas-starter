959cb59f3438b14a767e1d3df91d32d5f689d8d7
Noah Milberger <n.milberger@gmail.com>
2023-11-03 12:12:58 -0400

    Small updates to UI of ManageSubscriptionButton.

--- app/subscription/ManageSubscriptionButton.tsx
+++ app/subscription/ManageSubscriptionButton.tsx
@@ -23,11 +23,8 @@ export default function ManageSubscriptionButton({ session }: Props) {
   };
 
   return (
-    <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-      <p className="pb-4 sm:pb-0">Manage your subscription on Stripe.</p>
-      <Button isDisabled={!session} onClick={redirectToCustomerPortal}>
-        Open customer portal
-      </Button>
-    </div>
+    <Button isDisabled={!session} onClick={redirectToCustomerPortal}>
+      Manage Subscription
+    </Button>
   );
 }
