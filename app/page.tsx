471721c2524d2761b4c2a31c2ebf7dcb56237784
Noah Milberger <n.milberger@gmail.com>
2023-11-03 12:12:05 -0400

    Updated homepage to redirect to playground when user is signed in.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import {
 import Header from '@/components/feature-home/ui/Header';
 import Instructions from '@/components/feature-home/ui/Intstructions';
 import Pricing from '@/components/ui/Pricing';
-import { Stack } from '@chakra-ui/react';
+import { Flex, Stack } from '@chakra-ui/react';
+import { redirect } from 'next/navigation';
 
 export default async function PricingPage() {
   const [session, products, subscription] = await Promise.all([
@@ -15,18 +16,20 @@ export default async function PricingPage() {
     getSubscription()
   ]);
 
+  if (session?.user) {
+    return redirect('/playground');
+  }
+
   return (
     <Stack w="full" pb={16}>
       <Header />
       <Instructions />
-      {!session?.user && (
-        <Pricing
-          session={session}
-          user={session?.user}
-          products={products}
-          subscription={subscription}
-        />
-      )}
+      <Pricing
+        session={session}
+        user={session?.user}
+        products={products}
+        subscription={subscription}
+      />
     </Stack>
   );
 }
